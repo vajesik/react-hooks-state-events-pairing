0786a17ae170e80f54dbcd2fb88d7e0e8cf78f71
Vanessa Jesik <vajesik@gmail.com>
2023-08-28 20:53:00 -0600

    completed app without bonuses

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,59 @@
 import video from "../data/video.js";
+import React, { useState } from "react";
+import Votes from "./Votes.js";
+import Comments from "./Comments.js";
 
 function App() {
   console.log("Here's your data:", video);
+  const {
+    id,
+    title,
+    embedUrl,
+    views,
+    createdAt,
+    upvotes,
+    downvotes,
+    comments,
+  } = video;
+  const [upVotes, setUpVotes] = useState(upvotes);
+  const [downVotes, setDownVotes] = useState(downvotes);
+  const [isHidden, setIsHidden] = useState(false);
+
+  function handleLikeClick() {
+    setUpVotes(upVotes => (upVotes += 1));
+  }
+  function handleDislikeClick() {
+    setDownVotes(downVotes => (downVotes += 1));
+  }
+
+  function onCommentClick() {
+    setIsHidden(isHidden => !isHidden);
+  }
 
   return (
     <div className="App">
       <iframe
         width="919"
         height="525"
-        src="https://www.youtube.com/embed/dQw4w9WgXcQ"
+        src={embedUrl}
         frameBorder="0"
         allowFullScreen
         title="Thinking in React"
+      />
+      <h1>{title}</h1>
+      <p>
+        {views} Views | Uploaded {createdAt}
+      </p>
+      <Votes
+        upVotes={upVotes}
+        onLikeClick={handleLikeClick}
+        downVotes={downVotes}
+        onDislikeClick={handleDislikeClick}
+      />
+      <Comments
+        comments={comments}
+        isHidden={isHidden}
+        onCommentClick={onCommentClick}
       />
     </div>
   );
